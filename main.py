73e3a5dc77a676f1ee20237b23d35642d1d703e8
yymin1022 <yymin1022@gmail.com>
2021-05-14 05:57:08 +0000

    Initialized Flask Structure

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_main():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 80)
