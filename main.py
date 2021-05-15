7bf6c7d512c3e517293042d2b5c72e20c5cd436a
yymin1022 <yymin1022@gmail.com>
2021-05-15 07:41:00 +0000

    Add Login Button Action

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def render_login():
     return render_template("login.html")
 
-@app.route("/auth")
+@app.route("/auth", methods=["GET", "POST"])
 def render_auth():
+    userID = request.form["userID"]
+    userPW = request.form["userPW"]
     return render_template("auth.html")
 
 @app.route("/main")
 def render_main():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 80)
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 80)
