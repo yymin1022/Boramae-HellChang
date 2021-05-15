408a4f7ad75bbb2974c0c0dd312e0bf3a3fe9843
yymin1022 <yymin1022@gmail.com>
2021-05-15 06:55:58 +0000

    Add Redirecting Structure

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def render_route():
-    return render_template('route.html')
+    return redirect(url_for("render_login"))
 
 @app.route("/login")
 def render_login():
