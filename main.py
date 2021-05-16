749318e8c0a2ab6af806d2cdf8c019f8e06402f1
yymin1022 <yymin1022@gmail.com>
2021-05-16 07:19:33 +0000

    Update Imports

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, redirect, render_template, request, url_for
 from firebase_admin import credentials, firestore
 import firebase_admin
 
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
