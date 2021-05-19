9eabb004e63ec066864d6a03c16cb462f6e349b9
yymin1022 <yymin1022@gmail.com>
2021-05-19 05:02:06 +0000

    Update Imports

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 from firebase_admin import credentials, firestore
 import firebase_admin
